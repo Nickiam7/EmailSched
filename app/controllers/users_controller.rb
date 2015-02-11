bbebcea84d2368be632fe44dcab5f1298488992a
Nick McNeany <nmcneany@gmail.com>
2015-02-11 07:47:11 -0600

    Initial

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+	before_filter :admin, only: :show
+
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	private
+		def admin
+			@user = current_user
+			unless user_signed_in? && @user.admin == true
+				redirect_to root_path
+				flash[:alert] = "You do not have permission to access this page."
+			end
+		end
+end
