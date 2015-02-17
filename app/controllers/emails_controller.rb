1c98e1d04ad77e865d92e1c84ce82933ff6ee30f
Nick McNeany <nmcneany@gmail.com>
2015-02-17 07:38:40 -0600

    Added Navbar

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,6 +1,6 @@
 class EmailsController < ApplicationController
   before_action :set_email, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:index, :show]
   respond_to :html
 
   def index
